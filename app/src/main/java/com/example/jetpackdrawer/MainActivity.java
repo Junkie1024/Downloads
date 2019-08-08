dbead8a97366c3c9f2227d2e11d0ae65c280e536
1894082 <ymodi3859@gmail.com>
2019-08-08 16:54:09 -0400

    jetpack drawer

---
+++ app/src/main/java/com/example/jetpackdrawer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.jetpackdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    public NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupNavigation();
+    }
+
+    public void setupNavigation() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//navigation to default fragment
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+
+        navigationView = findViewById(R.id.navigationview);
+
+        navController = Navigation.findNavController(this, R.id.host_fragment);//navcontroller helps navigate from fragment to other fragment
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.host_fragment),drawerLayout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+        @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem menuItem){
+            menuItem.setCheckable(true);
+
+            drawerLayout.closeDrawers();
+
+            int id = menuItem.getItemId();
+
+            switch (id)
+            {
+                case R.id.first:
+                    navController.navigate(R.id.firstFragment);
+                    break;
+                case R.id.second:
+                    navController.navigate(R.id.secondFragment);
+                    break;
+                case R.id.third:
+                    navController.navigate(R.id.thirdFragment);
+                    break;
+            }
+
+            return true;
+        }
+    }
